111bb7c4a520c58b82651ab894b177bb886cb6cb
C-Ritz <65645811+C-Ritz@users.noreply.github.com>
2022-11-22 01:38:24 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 'use strict'
+
+document.addEventListener('click', function (e) {
+    const mbArr = ["m1", "m2", "m3", "m4", "m5", "m6"];
+
+    for (let i = 0; i < mbArr.length; i++) {
+        var str = mbArr[i];
+        console.log(str);
+        var mb = document.getElementById(mbArr[i]);
+        mb.classList.remove('glow');
+    }
+
+    var mBtn = document.getElementById(this.activeElement.getAttribute('id'));
+
+    mBtn.classList.add('glow');
+});
