a82bbc3348495e4ff717c7dc49158590313d97a6
DillonGriffin <70865787+DillonGriffin@users.noreply.github.com>
2022-11-22 01:41:10 -0600

    Update main.css

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var port = process.env.PORT || 1337;
 var fs = require('fs');
 
 http.createServer(function (req, res) {
+    console.log(req.url);
     switch (req.url) {
         case "/main.css":
             fs.readFile(__dirname + '/main.css', function (error, data) {
@@ -12,6 +13,19 @@ http.createServer(function (req, res) {
                 res.end();
             });
             break;
+        case "/Images/splash-1.jpeg":
+            var s = fs.createReadStream(__dirname + '/Images/splash-1.jpeg');
+            s.on('open', function () {
+                res.setHeader('Content-Type', 'image/jpeg');
+                s.pipe(res);
+            });
+            break;
+        case "/app.js":
+            fs.readFile(__dirname + '/app.js', function (error, data) {
+                res.writeHead(200, { 'Content-Type': 'application/javascript' });
+                res.write(data);
+                res.end();
+            });
         default:
             fs.readFile(__dirname + '/index.html', function (error, data) {
                 if (error) {
